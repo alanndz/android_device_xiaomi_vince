34998dbe1aaa54dcdd95b41d8d5b6cf5be883e29
alanndz <alanmahmud0@gmail.com>
2019-03-14 19:43:40 +0000

    vince : colt

--- prop.mk
+++ prop.mk
@@ -214,9 +214,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 persist.debug.wfd.enable=1 \
 persist.sys.wfd.virtual=0
 
-PRODUCT_PROPERTY_OVERRIDES += \
-ro.xenonhd.maintainer=rama982
-
 # Unsorted properties
 PRODUCT_PROPERTY_OVERRIDES += \
 keyguard.no_require_sim=true \
